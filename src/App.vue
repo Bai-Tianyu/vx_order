7d84cf7deb004d4f5b1776439a74c84b0733e00d
Tianyu <bty0609666@outlook.com>
2017-07-12 09:19:59 +0800

    the first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"></v-header>
+    <keep-alive>
+      <router-view :seller="seller"></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header'
+import axios from 'axios'
+
+const ERR_OK = 0
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      seller: {}
+    }
+  },
+  created() {
+    axios.get('static/data.json').then((res) => {
+      this.seller = res.data.seller
+    })
+  },
+  components: {
+    'v-header': header
+  }
+}
+</script>
+
+<style lang="stylus">
+  @import './common/stylus/index'
+</style>
